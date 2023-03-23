214a4194a70ec5a1a3737885382453643f63a13e
ironyhouse <ironylait@gmail.com>
2023-03-23 14:30:43 +0100

    update react/like app

--- React/likes/src/App.tsx
+++ React/likes/src/App.tsx
@@ -1,30 +1,45 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
 
 function App() {
   const [like, setLike] = useState(100);
   const [dislike, setDislike] = useState(50);
-  const [isLikeClick, setLikeClick] = useState(false);
+  const [isLikeClick, setLikeClick] = useState<null | boolean>(null);
 
   const handleLikeClick = () => {
-    setLikeClick(true)
-
-    if (!isLikeClick) {
+    if (isLikeClick === null) {
       setLike((state) => ++state);
-      setLikeClick(true)
+      setLikeClick(true);
+      return;
     }
+
+    if (isLikeClick === false) {
+      setLike((state) => ++state);
+      setDislike((state) => --state);
+      setLikeClick(true);
+      return;
+    }
+
+    setLike((state) => --state);
+    setLikeClick(null);
   };
 
   const handleDislikeClick = () => {
-    setLikeClick(false)
-
-    if (!isLikeClick) {
+    if (isLikeClick === null) {
       setDislike((state) => ++state);
+      setLikeClick(false);
+      return;
     }
 
+    if (isLikeClick) {
+      setLike((state) => --state);
+      setDislike((state) => ++state);
+      setLikeClick(false);
+      return;
+    }
 
+    setDislike((state) => --state);
+    setLikeClick(null);
   };
 
   return (
@@ -33,10 +48,16 @@ function App() {
         src="https://static.wikia.nocookie.net/dota2_gamepedia/images/0/0d/Outworld_Destroyer_Lore.png/revision/latest?cb=20210728220859"
         alt="Outworld destroyer"
       />
-      <button onClick={handleLikeClick} className={isLikeClick ? ' button' : ''}>
+      <button
+        onClick={handleLikeClick}
+        className={`button  ${isLikeClick ? 'like' : ''}`}
+      >
         <span>Like | {like}</span>
       </button>
-      <button onClick={handleDislikeClick} className={'button '}>
+      <button
+        onClick={handleDislikeClick}
+        className={`button  ${isLikeClick === false ? 'dislike' : ''}`}
+      >
         <span>Dislike | {dislike}</span>
       </button>
     </div>
